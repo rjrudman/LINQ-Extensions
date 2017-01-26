856b34df652a4119fedd2ea8a79ccc39bfdd8234
Robert Rudman <rjrudman@gmail.com>
2017-01-26 17:10:28 +1100

    Initial commit - LazyMaterializer

---
+++ src/LINQ Extensions.Test/LazyMaterializerTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LINQ_Extensions.Test
+{
+	[TestFixture]
+    public class LazyMaterializerTest
+	{
+		private readonly List<int> _createdItems = new List<int>();
+
+		[SetUp]
+	    public void Setup()
+	    {
+			_createdItems.Clear();
+	    }
+
+		[Test]
+	    public void TestItemsNotCreatedTwiceSingle()
+		{
+			var src = GetCollection();
+
+			var lazyMaterializer = new LazyMaterializer<int>(src);
+
+			var first = lazyMaterializer.First();
+			var firstAgain = lazyMaterializer.First();
+
+			Assert.AreEqual(1, _createdItems.Count);
+			Assert.AreEqual(first, firstAgain);
+		}
+
+		[Test]
+		public void TestItemsNotCreatedTwiceMultiple()
+		{
+			var src = GetCollection();
+
+			var lazyMaterializer = new LazyMaterializer<int>(src);
+
+			var first = lazyMaterializer.Take(5).ToList();
+			var second = lazyMaterializer.Take(3).ToList();
+
+			Assert.AreEqual(5, _createdItems.Count);
+			Assert.True(src.Take(5).SequenceEqual(first));
+			Assert.True(first.Take(3).SequenceEqual(second));
+		}
+
+		[Test]
+	    public void TestEnumerableLazilyEvaluated()
+	    {
+			var src = GetCollection();
+
+			var lazyMaterializer = new LazyMaterializer<int>(src);
+
+			lazyMaterializer.First();
+			
+			Assert.AreEqual(1, _createdItems.Count);
+		}
+
+
+		[Test]
+		public void TestEnumerableLazilyEvaluated_2()
+		{
+			var src = GetCollection();
+
+			var lazyMaterializer = new LazyMaterializer<int>(src);
+
+			lazyMaterializer.Take(5);
+
+			Assert.AreEqual(0, _createdItems.Count);
+		}
+
+		private IEnumerable<int> GetCollection()
+	    {
+		    var i = 0;
+		    while (true)
+		    {
+				_createdItems.Add(i);
+			    yield return i++;
+		    }
+	    }
+    }
+}
---
+++ src/LINQ Extensions/LazyMaterializer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LINQ_Extensions
+{
+	/// <summary>
+	/// Used to lazily materialize a collection. 
+	/// Multiple enumerations of this will only fetch from the underlying source as needed.
+	/// </summary>
+	public class LazyMaterializer<T> : IEnumerable<T>
+	{
+		private readonly IEnumerator<T> _sourceEnumerator;
+		private readonly List<T> _buffer = new List<T>();
+		public LazyMaterializer(IEnumerable<T> source)
+		{
+			_sourceEnumerator = source.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			return new LazyMaterializerEnumerator(_sourceEnumerator, _buffer);
+		}
+
+		private class LazyMaterializerEnumerator : IEnumerator<T>
+		{
+			private readonly IEnumerator<T> _sourceEnumerator;
+			private readonly List<T> _buffer;
+			private int _index = -1;
+
+			public LazyMaterializerEnumerator(IEnumerator<T> sourceEnumerator, List<T> buffer)
+			{
+				_sourceEnumerator = sourceEnumerator;
+				_buffer = buffer;
+			}
+
+			public bool MoveNext()
+			{
+				if (_buffer.Count > ++_index)
+					return true;
+
+				var didMoveNext = _sourceEnumerator.MoveNext();
+				if (didMoveNext)
+					_buffer.Add(_sourceEnumerator.Current);
+
+				return didMoveNext;
+			}
+
+			public void Reset()
+			{
+				_index = 0;
+				_sourceEnumerator.Reset();
+			}
+
+			public T Current => _index < _buffer.Count ? _buffer[_index] : _sourceEnumerator.Current;
+
+			object IEnumerator.Current => Current;
+
+			public void Dispose()
+			{
+				_sourceEnumerator.Dispose();
+			}
+		}
+	}
+}
